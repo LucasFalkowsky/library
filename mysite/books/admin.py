6d145530b493c84e9883c3969c49ac9f0baac64d
Lucas <lucas.falkowsky@typedigital.de>
2026-03-03 12:01:17 +0100

    chore: project setup

--- mysite/books/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-@admin.register(models.Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author_key')
-
-admin.site.register(models.Author)
-admin.site.register(models.Genre)
